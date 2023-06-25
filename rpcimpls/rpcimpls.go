e10f8477be28869245e9a733b774c41c9b264625
Quinn Caverly <quinncaverly@gmail.com>
2023-06-25 19:01:17 -0400

    Add ConnectToGoWritePod to rpcimpls

--- rpcimpls/rpcimpls.go
+++ rpcimpls/rpcimpls.go
@@ -1,17 +1,25 @@
 package rpcimpls
 
 import (
-    "net"
+	"net"
+	"net/rpc"
 )
 
 // this enables the ports which pods are listening on to be visible from one lcoation
 func CreatePodListener() (net.Listener, error) {
-    listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		return nil, err
+	}
+
+	return listener, nil
+}
+
+func ConnectToGoWritePod() (*rpc.Client, error) {
+	client, err := rpc.Dial("tcp", "go-write-service:8080")
 	if err != nil {
         return nil, err
 	}
 
-    return listener, nil
+    return client, err
 }
-
-
